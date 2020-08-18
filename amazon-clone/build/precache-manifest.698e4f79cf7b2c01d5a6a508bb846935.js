556373b0e1625238274c0cc536438a514cf682cf
Jordan Molina <jordanmichaelmolina3@gmail.com>
2020-08-18 13:01:22 -0500

    deployment

---
+++ amazon-clone/build/precache-manifest.698e4f79cf7b2c01d5a6a508bb846935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2301db9385249b84f0cbc70065255a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6255deefe09a5233c4cc",
+    "url": "/static/css/main.d945bbf8.chunk.css"
+  },
+  {
+    "revision": "b699f79f23d345667f6d",
+    "url": "/static/js/2.d0a107cf.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.d0a107cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6255deefe09a5233c4cc",
+    "url": "/static/js/main.11a1c694.chunk.js"
+  },
+  {
+    "revision": "7646288671f8458d76de",
+    "url": "/static/js/runtime-main.49627031.js"
+  }
+]);
